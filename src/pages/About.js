6167a23b1c1f6b81dd3b9aed429c5c8c9d750956
ValentinBusnel <valentin.busnel@epitech.eu>
2021-04-28 12:00:33 +0200

    updtae contact5

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
       <ButtonsBottom left='project-1' right='Contact' />
     <div className='container'>
       <h1 className='title_about'>Studio Munoz</h1>
-      <h2 className='fonction'>UI / UX Designer and Developer</h2>
+      <h2 className='fonction'>Graphic Designer</h2>
       <p className='description'>Been from a Venezuelan origin and in France for more than three years, I was able to discover a whole new country and acquire a dual culture. My experiences have brought me great personal enrichment and have led me to be much more autonomous and independent.
           <br/><br/>I am a Master Design Student. I am interested in editorial, fashion, visual identity, and typography and would like to collaborate with photographers. I seek to explore new ways of pushing boundaries, always looking for the new, the daring, to create something iconic, unique, special.
       </p>
