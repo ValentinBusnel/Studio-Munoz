cc4f22416a4fe2193806d23bd31087012fde342e
ValentinBusnel <valentin.busnel@epitech.eu>
2021-04-28 10:54:42 +0200

    updtae about

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,16 +4,23 @@ import ButtonsBottom from '../components/ButtonsBottom'
 
 const About = () => {
   return (
-    <div className='main'>
+    <div className='about-full'>
       <Navbar />
       <ButtonsBottom left='project-1' right='Contact' />
+    <div className='about-section'>
       <div className='about-main'>
-      <div className='about'>
+      <div className='inner-container'>
         <h1>About me</h1>
-        <p>Been from a Venezuelan origin and in France for more than three years, I was able to discover a whole new country and acquire a dual culture. My experiences have brought me great personal enrichment and have led me to be much more autonomous and independent.
+        <p className='text'>Been from a Venezuelan origin and in France for more than three years, I was able to discover a whole new country and acquire a dual culture. My experiences have brought me great personal enrichment and have led me to be much more autonomous and independent.
         <br /><br />I am a Master Design Student. I am interested in editorial, fashion, visual identity, and typography and would like to collaborate with photographers. I seek to explore new ways of pushing boundaries, always looking for the new, the daring, to create something iconic, unique, special.</p>
+        <div className='skills'>
+          <span>fdffef</span>
+          <span>fdffef</span>
+          <span>fdffef</span>
+        </div>
       </div>
       </div>
+    </div>
     </div>
   )
 }
